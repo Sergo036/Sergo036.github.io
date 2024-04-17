ce93434b5123462a407d4a3130f51cff29e8f07d
Sergo036 <nunchi851@gmail.com>
2024-04-17 09:15:33 +0500

    Upload Files

---
+++ assets/AboutPage-BTuvx1SL.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CUxs0kOx.js";const e="___page_title_1qzmg_1",_={__page_title:e};function a(){return t.jsx("h1",{className:_.__page_title,children:"О нас"})}export{a as default};
